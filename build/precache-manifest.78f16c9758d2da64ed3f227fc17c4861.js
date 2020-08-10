f64ef63625e9aeb76de6e34043cb6c3bfe8d0748
Evan Williams <edw@evanwilliams.info>
2020-08-10 11:17:03 -0700

    removing assets that were not for this project

---
+++ build/precache-manifest.78f16c9758d2da64ed3f227fc17c4861.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755f709dfce1415a179d87c3201b68c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8785cb339ec6946dd082",
+    "url": "/static/css/main.e012759c.chunk.css"
+  },
+  {
+    "revision": "599f081358519c2d3a40",
+    "url": "/static/js/2.f1cafe95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f1cafe95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8785cb339ec6946dd082",
+    "url": "/static/js/main.c803fd05.chunk.js"
+  },
+  {
+    "revision": "c2943550dcf44726d08c",
+    "url": "/static/js/runtime-main.dee1cd4b.js"
+  },
+  {
+    "revision": "4fd638a635e1b6cbd62a64fc717694fa",
+    "url": "/static/media/Rubik-LightItalic.4fd638a6.ttf"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "/static/media/Rubik-Medium.c87313aa.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/static/media/Rubik-Regular.b3d0902b.ttf"
+  }
+]);
